8520224d683caae088f7e2fed61df164ff602782
FieBee <phuonglf98@gmail.com>
2022-09-28 11:33:36 +0700

    aki

---
+++ src/main/java/com/example/th2springbootexception/Th2SpringBootExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.th2springbootexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Th2SpringBootExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Th2SpringBootExceptionApplication.class, args);
+    }
+
+}
